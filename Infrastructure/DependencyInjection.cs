2fbd1e746c0d9a0d3f6e89a3335c82bac53b8922
mohammad <mohamedwork216@gmail.com>
2025-05-14 08:10:30 +0300

    add infrastructure DI

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection {
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Register DbContext
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        // Register Repositories
+        // services.AddScoped<IProductRepository, ProductRepository>();
+        // Add other services...
+        return services;
+    }
+}
